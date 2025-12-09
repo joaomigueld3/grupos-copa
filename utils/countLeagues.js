dafba747813a9e7dec814497c02f4563a0e8e805
João Miguel <joaomigueld3@gmail.com>
2025-12-09 01:10:31 -0300

    mapping frequencies

--- utils/countLeagues.js
+++ utils/countLeagues.js
@@ -42,11 +42,23 @@ console.log(addArrayReturnBool(arrayNums, 4), arrayNums)
 
 function ligasFrequenciasDumb (selecoes) {
     console.log("--------ligasFrequenciasDumb----------")
-    const setLigas = new Set()
-    for (let i=0; i < selecoes.length; i++) {
-        const aux = setLigas.add(selecoes[i])
-        //console.log(aux)
-    }
+    const stackLiga = []
+    const stackFreq = []
+    selecoes.forEach(element => {
+        let freq = 0
+        if (!stackLiga.includes(element.liga)) {
+            stackLiga.push(element.liga)
+            stackFreq.push(freq++)
+        }
+        let index = stackLiga.indexOf(element.liga)
+        stackFreq[index]++
+    });
+    console.log(stackLiga, "\n", stackFreq)
+
+
+    const pares = stackLiga.map((liga, i) => [liga, stackFreq[i]])
+    const result = Object.fromEntries(pares)
+    return result
    }
 console.log(ligasFrequenciasDumb(selecoes))
 
@@ -54,10 +66,10 @@ const obterFrequencias = (array, atributo) => {
     const contagem = {}
     for (const obj of array) {
         const valor = obj[atributo]
-        if(contagem[valor]) {
-            contagem[valor] =+ 1
+        if(!contagem[valor]) {
+            contagem[valor] = 0
         }
-        contagem[valor] = contagem[valor]
+        contagem[valor]++    
         
     }
 
@@ -68,7 +80,7 @@ const obterFrequencias = (array, atributo) => {
 
 }
 
-/*
+
 const estatisticas = obterFrequencias(selecoes, 'liga');
 console.log("--------////----------")
-console.log(estatisticas); */
+console.log(estatisticas);
